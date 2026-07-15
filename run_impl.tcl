3b6eefeb6909a88bc18a1336fd370a94a31b5e04
KaustubhNayak005 <nayakkaustubh@gmail.com>
2026-07-15 11:49:31 +0530

    Update Vivado build configuration

--- run_impl.tcl
+++ run_impl.tcl
@@ -1,4 +1,4 @@
-open_project <repo>/riscv_pipeline_offline/riscv_pipeline_offline.xpr
+open_project [file dirname [file normalize [info script]]]/riscv_pipeline_offline/riscv_pipeline_offline.xpr
 
 # Reset runs
 reset_run synth_1
--- run_impl_gui.tcl
+++ run_impl_gui.tcl
@@ -1,4 +1,4 @@
-open_project <repo>/riscv_pipeline_offline/riscv_pipeline_offline.xpr
+open_project [file dirname [file normalize [info script]]]/riscv_pipeline_offline/riscv_pipeline_offline.xpr
 reset_run synth_1
 set_property strategy Flow_RuntimeOptimized [get_runs synth_1]
 launch_runs impl_1 -to_step write_bitstream -jobs 4
